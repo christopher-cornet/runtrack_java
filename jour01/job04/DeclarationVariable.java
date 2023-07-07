d59ebe778f33d7cfaca29126ec4b9e1aa711833f
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-07-08 00:33:57 +0200

    Add files via upload

---
+++ jour01/job04/DeclarationVariable.java
@@ -0,0 +1,7 @@
+public class DeclarationVariable {
+    public static void main(String[] args) {
+        int value1, value2, value3;
+        value1 = value2 = value3 = 50;
+        System.out.println(value1 + value2 + value3);
+    }
+}
---
+++ jour01/job05/Calcul.java
@@ -0,0 +1,8 @@
+public class Calcul {
+    public static void main(String[] args) {
+        int nombre = 7;
+        System.out.println(nombre);
+        System.out.println(nombre += 3);
+        System.out.println(nombre * 5);
+    }
+}
