b885e93b7308e2005a76ad91e3c0bedb6128ed33
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-07-28 22:46:28 +0200

    Add files via upload

--- jour02/job12/job12.java
+++ jour02/job12/job12.java
@@ -1,14 +1,14 @@
 public class job12 {
   public static void main(String[] args) {
     int count = 0;
-      for (int i = 1; i <= 6; i++) {
-        for (int j = 1; j <= 6; j++) {
-          for (int k = 1; k <= 6; k++) {
-            System.out.println(i + " " + j + " " + k);
-            count++;
-          }
+    for (int i = 1; i <= 6; i++) {
+      for (int j = 1; j <= 6; j++) {
+        for (int k = 1; k <= 6; k++) {
+          System.out.println(i + " " + j + " " + k);
+          count++;
         }
       }
-      System.out.println("Le nombre total de combinations est : " + count);
+    }
+    System.out.println("Le nombre total de combinations est : " + count);
   }
 }
